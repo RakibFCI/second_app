012aaf30b47e8cd6cd2dbaf5acb370d942654495
RakibFCI <nazrulislamfci14@gmail.com>
2022-12-03 16:12:07 +0600

    camaro

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Row(
+              children: [
+                IconButton(
+                    onPressed: (){},
+                    icon: NetworkImage(""))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
